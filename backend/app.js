46cb8b7487f7f3bc1e615bd515e349daa8ac1e6e
Johnny <thejohnnytran27@gmail.com>
2021-01-21 15:35:43 -0600

    first commit, set up backend server

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+const routes = require('./routes');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+if (!isProduction) {
+  app.use(cors());
+}
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && 'Lax',
+      httpOnly: true,
+    },
+  })
+);
+app.use(routes);
+
+module.exports = app;
