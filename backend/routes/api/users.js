2b66ad0301d79107dcceeb8a47bf5ebd88815c88
Johnny <thejohnnytran27@gmail.com>
2021-01-21 17:29:05 -0600

    complete and test user signup route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,31 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+router.post(
+  '',
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    if (!user) {
+      const err = new Error('Signup failed');
+      err.status = 401;
+      err.title = 'Signup failed';
+      err.errors = ['Could not create user with provided information.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  })
+);
 
 module.exports = router;
