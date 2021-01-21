e3ee17371fa5909113836acc86a963e9d29c7288
Johnny <thejohnnytran27@gmail.com>
2021-01-21 17:16:47 -0600

    complete login route, test valid and invalid credentials

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function (req, res) {
   res.json({ requestBody: req.body });
