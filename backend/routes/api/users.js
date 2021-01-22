b1782bc93c66bb074ccc8177b58379b129a10a15
Johnny <thejohnnytran27@gmail.com>
2021-01-22 12:06:14 -0600

    set up redux store

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,12 +3,32 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username').not().isEmail().withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Please provide a password with 6 characters or more.'),
+  handleValidationErrors,
+];
 
 // Signup /api/users
 router.post(
   '',
+  validateSignup,
   asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,10 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function (req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
 module.exports = router;
+
+//TEST UTILITY
+// router.get('/hello/world', function (req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
