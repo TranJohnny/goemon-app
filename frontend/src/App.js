e2da575817ebf3be5bdefcc3f36d4334ec89f3ab
Johnny <thejohnnytran27@gmail.com>
2021-01-22 15:22:17 -0600

    create signupForm component, test validation and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage';
 import * as sessionActions from './store/session';
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/login">
           <LoginFormPage />
+        </Route>
+        <Route path="/signup">
+          <SignupFormPage />
         </Route>
       </Switch>
     )
