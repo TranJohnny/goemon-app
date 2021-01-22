02cbba4148f17836e187a9f80482f0ef98b2b7c7
Johnny <thejohnnytran27@gmail.com>
2021-01-22 12:54:39 -0600

    create loginForm component, connect to redux store and db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,16 @@
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route exact path="/">
+        <h1>Hello from App</h1>
+      </Route>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
