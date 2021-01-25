20234a5d11c5f5613e115c1e21be3c531d280b51
Johnny <thejohnnytran27@gmail.com>
2021-01-24 18:07:09 -0600

    set up and test LoginModal

--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -1,8 +1,8 @@
-import React, { useContext, useRef, useState, useEffect } from 'react';
+import { createContext, useContext, useRef, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.css';
 
-const ModalContext = React.createContext();
+const ModalContext = createContext();
 
 export function ModalProvider({ children }) {
   const modalRef = useRef();
